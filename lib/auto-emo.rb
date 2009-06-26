b21965b82c126b5a1a3ba24e018c5bf648461348
Edward Ocampo-Gooding <edward@edwardog.net>
2009-06-25 23:38:48 -0400

    Changed main class to be AutoEmo::Poem

--- lib/auto-emo.rb
+++ lib/auto-emo.rb
@@ -1,22 +1,18 @@
-#!/usr/bin/env ruby
-#
-#  Created by Edward Ocampo-Gooding on 2007-01-31.
-
-require "rubygems"
-
-class EmoPoem
-  File.open(File.join(File.dirname(__FILE__), 'poems')) do |f|
-    @@poems = Marshal.load(f)
-  end
-  
-  def self.generate
-    length = 4 + rand(7)
-    lines = []
-
-    length.times do
-      lines << @@poems[rand(@@poems.size)]
+module AutoEmo
+  class Poem
+    File.open(File.join(File.dirname(__FILE__), 'poems')) do |f|
+      @@poems = Marshal.load(f)
     end
 
-    lines.join("\n")
+    def self.generate
+      length = 4 + rand(7)
+      lines = []
+
+      length.times do
+        lines << @@poems[rand(@@poems.size)]
+      end
+
+      lines.join("\n")
+    end
   end
 end
